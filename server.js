dd6630faf60734777822b375f259328bba85cea9
Abel García <agarcia@gabinete.mx>
2017-04-06 17:04:58 -0500

    agregando comentarios

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ var server = http.createServer(function (req,res) {
 	res.end('Hello World');
 }).listen(9000);
 
+// ir a la terminal y ejecutar este archivo con la siguinete linea de comandos /node server.js
+
 // ir al navegador y ejecutar la siguiente liga http://localhost:9000/
